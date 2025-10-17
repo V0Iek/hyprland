112258c05ac2ffbfdd95b2d767e72aa657a846bf
V0Iek <michal.bernas2005@gmail.com>
2025-10-17 14:48:13 +0200

    Integrated hyprlock with theme switcher

---
+++ .config/hypr/scripts/hyprlock-song.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+song_info=$(playerctl metadata --format "󰎆   {{title}}")
+
+echo "$song_info"
--- .config/scripts/theme-switcher.sh
+++ .config/scripts/theme-switcher.sh
@@ -7,8 +7,10 @@ fi
 
 IMAGE="$1"
 
-swww img "$IMAGE" --transition-type=center --transition-fps=60 --transition-step=255
+swww img "$IMAGE" --transition-type=any --transition-fps=60 --transition-step=255
 
 echo "$IMAGE"
 
 matugen image "$IMAGE"
+
+ln -sf "$HOME/.Wallpapers/$IMAGE" ~/.config/hypr/wallpaper
